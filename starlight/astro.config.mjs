3194efe24c4c64798bd6b7997e0578ebb5745ab0
Joe Ervin <joeerv123@outlook.com>
2026-02-01 18:52:14 -0500

    astro config

--- starlight/astro.config.mjs
+++ starlight/astro.config.mjs
@@ -4,22 +4,40 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://sesame-auth.com',
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Sesame',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/joe-ervin05/sesame' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: "Overview",
+					link: "/"
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: "1. Sessions",
+					autogenerate: { directory: 'sessions' }
 				},
+				{
+					label: "2. Rate Limiting",
+					autogenerate: { directory: "rate-limiting" }
+				},
+				{
+					label: "3. Inactivity Timeouts",
+					autogenerate: { directory: "inactivity-timeouts" }
+				},
+				{
+					label: "4. OAuth",
+					autogenerate: { directory: "oauth" }
+				},
+				{
+					label: "5. 2FA",
+					autogenerate: { directory: "mfa" }
+				},
+				{
+					label: "6. Passkeys",
+					autogenerate: { directory: "passkeys" }
+				}
 			],
 		}),
 	],
